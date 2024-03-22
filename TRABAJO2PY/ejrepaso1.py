711b3e7f8211c40fbfea4731117b4ec4c4c13810
Miqueas Lacaze <miqueaslacaze@gmail.com>
2024-03-22 08:35:21 -0300

    Agregando TP2

---
+++ TRABAJO2PY/ejrepaso1.py
@@ -0,0 +1,21 @@
+"""
+) Implementaremos una clase llamada Persona que tendrá como atributo (variable) su nombre y dos
+métodos (funciones), uno de dichos métodos inicializará el atributo nombre y el siguiente método mostrará
+en la pantalla el contenido del mismo.
+Definir dos objetos de la clase Persona.
+""" 
+
+class Persona:
+    def inicializar(self,nom):
+        self.nombre=nom
+
+    def imprimir(self):
+        print(f"Nombre", self.nombre)
+
+persona1=Persona()
+persona1.inicializar("andy")
+persona1.imprimir()
+
+persona2=Persona()
+persona2.inicializar("valen")
+persona2.imprimir()
